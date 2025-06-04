2197b59998b964b58b7263b6099dbc8a933e49e0
Felipe Martins Hulle <felipe.hulle@findes.org.br>
2025-06-04 11:01:54 -0300

    Improve the difficulty class and indetation

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 
 class Difficulty:
     def choosing_difficulty(self):
-        choose = ''
+
+        choose = -1
 
         chances = {
             1 : 8,
@@ -14,8 +15,7 @@ class Difficulty:
         while choose not in chances:
         
             try:
-                choose = input('Choose your difficulty (Write the name):\n1. Easy (8 chances)\n2. Medium (6 chances)\n3. Hard (4 chances)\n4. Impossible (3 chances)\n\n')
-            
+                choose = int(input('Choose your difficulty (Write the name):\n1. Easy (8 chances)\n2. Medium (6 chances)\n3. Hard (4 chances)\n4. Impossible (3 chances)\n\n'))
             except:
                 print('Choose a valid number')
         
@@ -46,16 +46,16 @@ class Game:
 
             if number_picked > magic_number:
                 chances += 1
-                print(f'The number is greater than Magic Number\n')
+                print(f'The number is greater than Magic Number')
                 continue
             elif number_picked < magic_number:
                 chances += 1
-                print(f'The number is less than Magic Number\n')
+                print(f'The number is less than Magic Number')
                 continue
             elif number_picked == magic_number:
                 return print('\n********YOU WONNNNNN!!!!!!********\nTHE NUMBER IS CORRECT')
             
-        return print('You lose, try again :(')
+        return print(f'\nYou lose, try again :(\nThe Magic Number was: {magic_number}')
             
 
 game_instance = Game()
