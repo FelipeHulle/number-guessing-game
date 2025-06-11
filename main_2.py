c94b809b23bc1eb1f3937b205e2970cc95d904a9
Felipe Hulle <felipehulle@hotmail.com>
2025-06-10 21:28:38 -0300

    improvement main_2

--- main_2.py
+++ main_2.py
@@ -7,3 +7,23 @@
 # | Difficulty        | Representa os níveis de dificuldade e suas regras.                             |
 # | Round             | Representa uma única rodada do jogo (lógica de adivinhação, tempo, resultado). |
 # | HintSystem        | Lógica de dicas, acoplada de forma opcional à rodada.                          |
+
+from enum import Enum
+
+class DifficultyLevel(Enum):
+
+    EASY        = 8
+    MEDIUM      = 6
+    HARD        = 4
+    IMPOSSIBLE  = 3
+
+class Difficulty:
+
+    @staticmethod
+    def get_chances(level: DifficultyLevel):
+        return level.value
+
+class PlayerInterface:
+
+    def get_difficulty():
+        pass
